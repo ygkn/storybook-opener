a1504fdd794091b1461a8da10a0289309e5a2b0f
ygkn <ygkn@users.noreply.github.com>
2023-06-13 02:20:53 +0900

    v0.0.3

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Storybook Opener
 
 A Visual Studio Code extension for effortless story opening
+
+1. Open folder containing Storybook configuration as root (`/.storybook` folder)
+2. Open component story format file in editor(`*.stories.{t,j}sx`, `*.mdx` etc. It depens your storybook config)
+3. **You can acesss storybook story in browser quickly!**
+   - Click [editor actions](https://code.visualstudio.com/api/ux-guidelines/editor-actions) button (you might see it in the editor toolbar)
+   - Click `Open Story` from editor context (right-click) menu
+   - Run `Storybook Opener: Open Story` from command palette
+
+## Features
+
+- [x] Respects your Storybook configuration (in `.storybook` folder)
+- [x] Respects your Story (titles, etc)
+
+> **Note**
+> This extension **does not** launch stroybook.
+>
+> Please run storybook before open stories.
--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,36 @@
 import * as vscode from "vscode";
+import { loadCurrentCsf } from "./loadCurrentCsf";
 import { loadStoryEntries } from "./loadStoryEntries";
 import { loadStoryIndexers } from "./loadStoryIndexers";
-import { loadCurrentCsf } from "./loadCurrentCsf";
 import { requireFromWorkSpace } from "./requireFromWorkspace";
+
+type WorkspaceCacheItem = {
+  storyIndexers: import("@storybook/types").StoryIndexer[];
+  entries: import("@storybook/types").NormalizedStoriesSpecifier[];
+};
+
+const workspaceCache = new Map<string, WorkspaceCacheItem>();
+
+const getOrFallbackFromWorkspaceCache = async (
+  key: string,
+  fallback: () => Promise<WorkspaceCacheItem>
+): Promise<WorkspaceCacheItem> => {
+  const cached = workspaceCache.get(key);
+
+  if (cached !== undefined) {
+    return cached;
+  }
+
+  const fallbackResult = await fallback();
+
+  workspaceCache.set(key, fallbackResult);
+
+  return fallbackResult;
+};
 
 export async function activate(
   context: vscode.ExtensionContext
 ): Promise<void> {
-  // TODO: update stuff when workspace updated
   // TODO: update config etc. when config file updated
   const workspaceUri = vscode.workspace.workspaceFolders?.[0]?.uri;
 
@@ -22,68 +45,103 @@ export async function activate(
 
   console.log(`storybook-opener: using storybook config "${configDir}"`);
 
-  const [storyIndexers, entries] = await Promise.all([
-    loadStoryIndexers(configDir).then((v) => {
-      console.log(
-        "storybook-opener: indexers loaded!",
-        v.map((indexer) => indexer.test.toString())
-      );
-      return v;
-    }),
-    loadStoryEntries(configDir, workingDir).then((v) => {
-      console.log(
-        "storybook-opener: entries loaded!",
-        v.map((entry) => entry.files)
-      );
-      return v;
-    }),
-  ]);
+  const { storyIndexers, entries } = await getOrFallbackFromWorkspaceCache(
+    workingDir,
+    async () => {
+      const [storyIndexers, entries] = await Promise.all([
+        loadStoryIndexers(configDir).then((v) => {
+          console.log(
+            "storybook-opener: indexers loaded!",
+            v.map((indexer) => indexer.test.toString())
+          );
+          return v;
+        }),
+        loadStoryEntries(configDir, workingDir).then((v) => {
+          console.log(
+            "storybook-opener: entries loaded!",
+            v.map((entry) => entry.files)
+          );
+          return v;
+        }),
+      ]);
+
+      return { storyIndexers, entries };
+    }
+  );
+
   console.log("storybook-opener: READY!!");
 
   const { toId } = requireFromWorkSpace(
     "@storybook/csf"
   ) as typeof import("@storybook/csf");
 
+  let storyUrl: string | null = null;
+
+  const setIsActiveEditorCsf = async (
+    editor: vscode.TextEditor | undefined
+  ): Promise<void> => {
+    if (editor === undefined) {
+      storyUrl = null;
+
+      vscode.commands.executeCommand(
+        "setContext",
+        "storybook-opener.isActiveEditorCsf",
+        false
+      );
+
+      return;
+    }
+
+    const absolutePath = editor.document.uri.fsPath;
+
+    const csf = await loadCurrentCsf(
+      workingDir,
+      absolutePath,
+      entries,
+      storyIndexers
+    );
+
+    if (csf?.meta.title === undefined) {
+      storyUrl = null;
+
+      vscode.commands.executeCommand(
+        "setContext",
+        "storybook-opener.isActiveEditorCsf",
+        false
+      );
+      return;
+    }
+
+    // TODO: get port number by config
+    storyUrl = `http://localhost:6006/?path=/story/${toId(csf.meta.title)}`;
+
+    console.log("set story url:", storyUrl);
+
+    vscode.commands.executeCommand(
+      "setContext",
+      "storybook-opener.isActiveEditorCsf",
+      true
+    );
+  };
+
+  context.subscriptions.push(
+    vscode.window.onDidChangeActiveTextEditor(setIsActiveEditorCsf)
+  );
+
+  setIsActiveEditorCsf(vscode.window.activeTextEditor);
+
   let disposable = vscode.commands.registerCommand(
     "storybook-opener.open",
     async () => {
-      const editor = vscode.window.activeTextEditor;
-
-      if (editor === undefined) {
+      if (storyUrl === null) {
         await vscode.window.showInformationMessage(
-          "No active editor found. Please focus to editor opening story."
+          "Please focus to editor opening story."
         );
-        return;
-      }
-
-      const absolutePath = editor.document.uri.fsPath;
-
-      const csf = await loadCurrentCsf(
-        workingDir,
-        absolutePath,
-        entries,
-        storyIndexers
-      );
-
-      if (csf === undefined) {
-        await vscode.window.showInformationMessage(
-          "Your current active editor is not story. Please focus to editor opening story."
-        );
-        return;
-      }
-
-      if (csf.meta.title === undefined) {
-        // TODO: notify to user
         return;
       }
 
       // TODO: open story which cursor active
-      // TODO: get port number by config
-      vscode.env.openExternal(
-        vscode.Uri.parse(
-          `http://localhost:6006/?path=/story/${toId(csf.meta.title)}`
-        )
-      );
+      vscode.env.openExternal(vscode.Uri.parse(storyUrl));
     }
   );
 
